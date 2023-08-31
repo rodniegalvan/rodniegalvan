b8f44cc36e5e579ec530031b3333da38008d7eed
Rodnie Galvan <rodniegalvan.sama@gmail.com>
2023-08-31 14:50:54 +0800

    first commit

---
+++ galvan-portfolio-starter-main/components/Nav.js
@@ -0,0 +1,47 @@
+// icons
+import {
+  HiHome,
+  HiUser,
+  HiViewColumns,
+  HiRectangleGroup,
+  HiChatBubbleBottomCenterText,
+  HiEnvelope,
+} from "react-icons/hi2";
+
+// nav data
+export const navData = [
+  { name: "home", path: "/", icon: <HiHome /> },
+  { name: "about", path: "/about", icon: <HiUser /> },
+  { name: "services", path: "/services", icon: <HiRectangleGroup /> },
+  { name: "work", path: "/work", icon: <HiViewColumns /> },
+  {
+    name: "testimonials",
+    path: "/testimonials",
+    icon: <HiChatBubbleBottomCenterText />,
+  },
+  {
+    name: "contact",
+    path: "/contact",
+    icon: <HiEnvelope />,
+  },
+];
+
+import { useRouter } from "next/router";
+import Link from 'next/link';
+
+const Nav = () => {
+  const router = useRouter();
+  const pathame = router.pathname;
+  return (
+    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed">
+      {}
+      <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-40 xl:px-0 h-80 xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
+        {navData.map((link, index) => {
+          return <Link href={link.path}>{link.icon}</Link>;
+        })}
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
