e35d8938ddd292c17253723943ee78d8cd09038a
Rodnie Galvan <rodniegalvan.sama@gmail.com>
2023-09-01 11:44:03 +0800

    Sept 1 2023

--- galvan-portfolio-starter-main/components/Avatar.js
+++ galvan-portfolio-starter-main/components/Avatar.js
@@ -1,5 +1,10 @@
+import Image from "next/image";
+
+
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return (<div className="hidden xl:flex xl:max-w-none">
+    <Image src={'/avatar.png'} width={737} height={678} alt="My Picture" className="translate-z-0 w-full h-full"/></div>);
 };
 
 export default Avatar;
