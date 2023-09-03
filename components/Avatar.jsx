97404aa9e9e02a36c2d0f692678ae128a3f272e9
Rodnie Galvan <rodniegalvan.sama@gmail.com>
2023-09-04 05:27:45 +0800

    Change root directory

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 
-
-
 const Avatar = () => {
   return (<div className="hidden xl:flex xl:max-w-none">
     <Image src={'/myBgPhoto.png'} width={737} height={678} alt="My Picture" className="translate-z-0 w-full h-full"/></div>);
