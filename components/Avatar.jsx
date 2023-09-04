1c4147d7759a525155b3f7cc3a1f5e3228d3f803
Rodnie Galvan <rodniegalvan.sama@gmail.com>
2023-09-04 14:47:49 +0800

    revert avatar

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -1,18 +1,8 @@
 import Image from "next/image";
-import image01 from "../public/myBgPhoto.png";
 
 const Avatar = () => {
-  return (
-    <div className="hidden xl:flex xl:max-w-none">
-      <Image
-        src={image01}
-        width={737}
-        height={678}
-        alt="My Picture"
-        className="translate-z-0 w-full h-full"
-      />
-    </div>
-  );
+  return (<div className="hidden xl:flex xl:max-w-none">
+    <Image src={'/avatar.png'} width={737} height={678} alt="My Picture" className="translate-z-0 w-full h-full"/></div>);
 };
 
 export default Avatar;
